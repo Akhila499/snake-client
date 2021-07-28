138ce69dead662a8ca9c8b88ce71e81ce3bfdc3b
Akhila499 <akhilathota499@gmail.com>
2021-07-28 15:20:27 -0400

    displayed name on the snake.

--- client.js
+++ client.js
@@ -5,16 +5,29 @@ const connect = function () {
   const conn = net.createConnection({
     host: '135.23.223.133',// IP address here,
     port: 50542// PORT number here,
-  });
+  }, 
+  // () => {
+  //   console.log("Successfully connected to game server")
+  // }
+  );
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on('data', function(data) {
     console.log(data);
+    
   })
+  
+  conn.on('connect',()=>{
+    console.log("Successfully connected to game server");
+    conn.write('Name: AKI');
+  })
+  
   return conn;
 };
 
 console.log("Connecting ...");
 connect();
+
+
 module.exports = { connect };
