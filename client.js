22ee504703f79a02d6d841afa985786a305dd3c8
Akhila499 <akhilathota499@gmail.com>
2021-07-28 20:08:14 -0400

    used constants for ip and port

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
 const net = require("net");
 
 // establishes a connection with the game server
+const { IP, PORT } = require("./constants");
+
 const connect = function () {
   const conn = net.createConnection({
-    host: '135.23.223.133',// IP address here,
+    host: IP,// IP address here,
     port: 50542// PORT number here,
   }, 
   // () => {
